e7234acfb7566aabcc81f2a5abec72b1b42725ca
yonatan molla <jhoni42002@yahoo.com>
2017-06-28 13:42:53 -0400

    signup page created

--- ClientFront/src/main/java/com/clientFront/controler/HomeController.java
+++ ClientFront/src/main/java/com/clientFront/controler/HomeController.java
@@ -1,7 +1,11 @@
 package com.clientFront.controler;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.clientFront.domain.User;
 
 @Controller
 public class HomeController {
@@ -14,5 +18,12 @@ public class HomeController {
 	@RequestMapping("/index")
 	public String index() {
 		return "index";
+	}
+
+	@GetMapping("/signup")
+	public String signup(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
+		return "signup";
 	}
 }
